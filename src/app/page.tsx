f510e2487efed5ccdb46547f0ea1e944f464a88b
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-10-23 14:57:31 +0530

    made landing page responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
     <div className="flex h-screen w-full items-center justify-center bg-black px-2 py-8">
       <BorderContainer>
         <h1
-          className={`${vintage.className} neonBlueText text-center text-[13rem] tracking-widest text-blue-500`}
+          className={`${vintage.className} neonBlueText t racking-widest text-center text-7xl text-blue-500 sm:text-9xl lg:text-[13rem]`}
         >
           Noesis
         </h1>
