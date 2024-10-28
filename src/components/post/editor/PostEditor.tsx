1a82cb785f977adde68adb82cf8a77a7e7a6234d
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-10-28 06:27:07 +0530

    added avatar and placeholder profile pic

--- src/components/post/editor/PostEditor.tsx
+++ src/components/post/editor/PostEditor.tsx
@@ -3,14 +3,25 @@
 import PostTypeDropdown from "./PostTypeDropdown";
 import { useState } from "react";
 import PostCommentry from "./PostCommentry";
+import Image from "next/image";
+import profilePlaceholder from "../../../assets/profile-placeholder.png";
+import { useSession } from "@/contexts/SessionProvider";
 
 const PostEditor = () => {
+  const { user } = useSession();
   const [value, setValue] = useState("");
 
   return (
     <div className="flex flex-col gap-12 border-b border-blue-400 p-6">
       <div className="flex items-center justify-between">
-        <div className="h-14 w-14 rounded-full bg-blue-600"></div>
+        <div className="relative h-14 w-14 rounded-full">
+          <Image
+            src={user.avatarUrl ? user.avatarUrl : profilePlaceholder}
+            alt="profile picture"
+            fill
+            className="object-cover"
+          />
+        </div>
         <PostTypeDropdown value={value} setValue={setValue} />
       </div>
       <PostCommentry value={value} />
