b53c3162404975c766cee5c01bf7fc5bc31caf70
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-11-22 06:15:04 +0530

    added loading components

---
+++ src/app/(main)/loading.tsx
@@ -0,0 +1,8 @@
+import { Loader2 } from "lucide-react";
+import React from "react";
+
+const Loading = () => {
+  return <Loader2 className="mx-auto my-10 animate-spin text-blue-500" />;
+};
+
+export default Loading;
