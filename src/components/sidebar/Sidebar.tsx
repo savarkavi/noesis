90bae2db354ebfa83b0f3250d78b0e3c2acd1037
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-11-22 06:16:24 +0530

    made sidebar responsive

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Loader2 } from "lucide-react";
 
 const Sidebar = () => {
   return (
-    <div className="sticky top-12 hidden h-fit w-full max-w-[400px] justify-center px-8 sm:flex md:w-full lg:block">
+    <div className="sticky top-12 hidden h-fit w-full max-w-[400px] justify-center px-8 md:w-full lg:flex">
       <Suspense
         fallback={<Loader2 className="mx-auto animate-spin text-blue-500" />}
       >
