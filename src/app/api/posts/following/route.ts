2cbae1476786e3e4c538d90b1f7cea6f6ca4fcee
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-11-28 17:44:17 +0530

    added postDataInclude type

--- src/app/api/posts/following/route.ts
+++ src/app/api/posts/following/route.ts
@@ -1,6 +1,6 @@
 import prisma from "@/lib/prisma";
 import { getCurrentSession } from "@/lib/session";
-import { PostPage, postWithUser } from "@/lib/types";
+import { PostPage, postDataInclude } from "@/lib/types";
 import { NextRequest } from "next/server";
 
 export async function GET(req: NextRequest) {
@@ -25,7 +25,7 @@ export async function GET(req: NextRequest) {
           },
         },
       },
-      include: postWithUser,
+      include: postDataInclude,
       orderBy: { createdAt: "desc" },
       take: pageSize + 1,
       cursor: cursor ? { id: cursor } : undefined,
