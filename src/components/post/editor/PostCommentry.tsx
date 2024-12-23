685eb6bc23c897a012d7ef76892c1fbf943f8de7
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-12-23 06:48:22 +0530

    feat: integrate file upload functionality with drag-and-drop support in PostCommentry component

--- src/components/post/editor/PostCommentry.tsx
+++ src/components/post/editor/PostCommentry.tsx
@@ -3,7 +3,7 @@
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
-import { cn } from "@/lib/utils";
+import { cn, validateFiles } from "@/lib/utils";
 import { useCreatePostMutation } from "@/lib/mutations/postMutations";
 import { toast } from "sonner";
 import useMediaUpload from "@/hooks/useMediaUpload";
@@ -13,6 +13,7 @@ import "swiper/css";
 import "swiper/css/navigation";
 import PreviewFiles from "./PreviewFiles";
 import PostEditorFooter from "./PostEditorFooter";
+import { useDropzone } from "@uploadthing/react";
 
 export interface previewFile {
   type: string;
@@ -32,6 +33,31 @@ const PostCommentry = ({ value }: { value: string }) => {
     isUploading,
     removeAttachment,
   } = useMediaUpload();
+
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop: (files) => {
+      const fileList = new DataTransfer();
+      files.forEach((file) => fileList.items.add(file));
+      const validFiles = validateFiles(fileList.files);
+
+      if (validFiles.length > 0) {
+        setPreviewFiles((prev) => [
+          ...prev,
+          ...validFiles.map((f) => {
+            return {
+              type: f.type,
+              url: URL.createObjectURL(f),
+              name: f.name,
+            };
+          }),
+        ]);
+        startUpload(validFiles);
+      }
+    },
+  });
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { onClick, ...rootProps } = getRootProps();
 
   const editor = useEditor({
     extensions: [
@@ -72,13 +98,16 @@ const PostCommentry = ({ value }: { value: string }) => {
 
   return (
     <div>
-      <EditorContent
-        editor={editor}
-        className={cn(
-          "text-white",
-          previewFiles.length > 0 && "border-b border-gray-400 pb-4",
-        )}
-      />
+      <div {...rootProps} className="w-full">
+        <EditorContent
+          editor={editor}
+          className={cn(
+            "py-4 text-white",
+            isDragActive && "rounded-xl px-2 outline-dashed outline-[0.5px]",
+          )}
+        />
+        <input {...getInputProps()} />
+      </div>
       {previewFiles.length > 0 && (
         <PreviewFiles
           previewFiles={previewFiles}
