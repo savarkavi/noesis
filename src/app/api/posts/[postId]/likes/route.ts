5d5d52adcbaaf3c91291cb4c3e7c30d239666201
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-12-25 06:05:15 +0530

    feat: implement API endpoints for managing post likes

---
+++ src/app/api/posts/[postId]/likes/route.ts
@@ -0,0 +1,111 @@
+import prisma from "@/lib/prisma";
+import { getCurrentSession } from "@/lib/session";
+import { PostLikesInfo } from "@/lib/types";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { postId: string } },
+) {
+  const { postId } = await params;
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const post = await prisma.post.findUnique({
+      where: { id: postId },
+      select: {
+        likes: {
+          where: {
+            userId: loggedInUser.id,
+          },
+          select: {
+            userId: true,
+          },
+        },
+        _count: {
+          select: {
+            likes: true,
+          },
+        },
+      },
+    });
+
+    if (!post) {
+      return Response.json({ error: "Post not found" }, { status: 404 });
+    }
+
+    const data: PostLikesInfo = {
+      totalLikes: post._count.likes,
+      isLiked: !!post.likes.length,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { postId: string } },
+) {
+  const { postId } = await params;
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.like.upsert({
+      where: {
+        userId_postId: {
+          userId: loggedInUser.id,
+          postId: postId,
+        },
+      },
+      create: {
+        userId: loggedInUser.id,
+        postId: postId,
+      },
+      update: {},
+    });
+
+    return new Response();
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { postId: string } },
+) {
+  const { postId } = await params;
+
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.like.deleteMany({
+      where: {
+        userId: loggedInUser.id,
+        postId: postId,
+      },
+    });
+
+    return new Response();
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
