f4ea38174b6a0ee48966e14f3e85fe5b5afba042
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-12-26 07:01:54 +0530

    feat: implement CRUD operations for post bookmarks in API

---
+++ src/app/api/posts/[postId]/bookmarks/route.ts
@@ -0,0 +1,96 @@
+import prisma from "@/lib/prisma";
+import { getCurrentSession } from "@/lib/session";
+import { PostBookmarkInfo } from "@/lib/types";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { postId: string } },
+) {
+  const { postId } = await params;
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const bookmark = await prisma.bookmark.findUnique({
+      where: {
+        userId_postId: {
+          userId: loggedInUser.id,
+          postId,
+        },
+      },
+    });
+
+    const data: PostBookmarkInfo = {
+      isBookmarked: !!bookmark,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { postId: string } },
+) {
+  const { postId } = await params;
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.bookmark.upsert({
+      where: {
+        userId_postId: {
+          userId: loggedInUser.id,
+          postId: postId,
+        },
+      },
+      create: {
+        userId: loggedInUser.id,
+        postId: postId,
+      },
+      update: {},
+    });
+
+    return new Response();
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { postId: string } },
+) {
+  const { postId } = await params;
+
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    await prisma.bookmark.deleteMany({
+      where: {
+        userId: loggedInUser.id,
+        postId: postId,
+      },
+    });
+
+    return new Response();
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
