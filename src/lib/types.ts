e0384b30a365d06bb3d4a0b0f1d408861caff537
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-12-27 11:22:24 +0530

    feat: add comment data inclusion and type definitions for comments

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -11,6 +11,11 @@ export const postDataInclude = {
   attachments: true,
   likes: true,
   bookmarks: true,
+  _count: {
+    select: {
+      comments: true,
+    },
+  },
 } satisfies Prisma.PostInclude;
 
 export const userSelect = {
@@ -28,13 +33,29 @@ export const userSelect = {
   },
 } satisfies Prisma.UserSelect;
 
+export const commentDataInclude = {
+  user: {
+    select: userSelect,
+  },
+};
+
 export type PostData = Prisma.PostGetPayload<{
   include: typeof postDataInclude;
 }>;
+
 export type UserData = Prisma.UserGetPayload<{ select: typeof userSelect }>;
+
+export type CommentData = Prisma.CommentGetPayload<{
+  include: typeof commentDataInclude;
+}>;
 
 export interface PostPage {
   posts: PostData[];
+  nextCursor: string | null;
+}
+
+export interface CommentPage {
+  comments: CommentData[];
   nextCursor: string | null;
 }
 
