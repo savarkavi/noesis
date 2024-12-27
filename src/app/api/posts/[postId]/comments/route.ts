73e14954f32e2fa79dbe6475cd20372f4297254c
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-12-27 11:22:57 +0530

    feat: implement GET route for fetching comments by post ID with pagination

---
+++ src/app/api/posts/[postId]/comments/route.ts
@@ -0,0 +1,43 @@
+import prisma from "@/lib/prisma";
+import { getCurrentSession } from "@/lib/session";
+import { commentDataInclude, CommentPage } from "@/lib/types";
+import { NextRequest } from "next/server";
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { postId: string } },
+) {
+  const { postId } = await params;
+  try {
+    const cursor = req.nextUrl.searchParams.get("cursor") || undefined;
+
+    const pageSize = 10;
+
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const comments = await prisma.comment.findMany({
+      where: { id: postId },
+      include: commentDataInclude,
+      orderBy: { createdAt: "desc" },
+      take: pageSize + 1,
+      cursor: cursor ? { id: cursor } : undefined,
+    });
+
+    const nextCursor =
+      comments.length > pageSize ? comments[pageSize].id : null;
+
+    const data: CommentPage = {
+      comments: comments.slice(0, pageSize),
+      nextCursor,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
