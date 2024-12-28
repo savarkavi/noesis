a2b7842fabb09081474ce247feccdf77655bf7ee
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-12-28 09:47:42 +0530

    feat: replace Dialog with AlertDialog for post deletion confirmation

--- src/components/post/DeletePostDialog.tsx
+++ src/components/post/DeletePostDialog.tsx
@@ -1,16 +1,17 @@
-import React from "react";
-import {
-  Dialog,
-  DialogContent,
-  DialogFooter,
-  DialogHeader,
-} from "../ui/dialog";
 import { Button } from "../ui/button";
 import { useDeletePostMutation } from "@/lib/mutations/postMutations";
 import { PostData } from "@/lib/types";
 import { toast } from "sonner";
 import { Loader2 } from "lucide-react";
-import { DialogTitle } from "@radix-ui/react-dialog";
+
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
 
 interface DeletePostDialogProps {
   open: boolean;
@@ -22,12 +23,15 @@ const DeletePostDialog = ({ open, onClose, post }: DeletePostDialogProps) => {
   const mutation = useDeletePostMutation();
 
   return (
-    <Dialog open={open} onOpenChange={onClose}>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Are you sure you want to delete this post?</DialogTitle>
-        </DialogHeader>
-        <DialogFooter>
+    <AlertDialog open={open} onOpenChange={onClose}>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>
+            Are you sure you want to delete this post?
+          </AlertDialogTitle>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
           <Button
             variant="destructive"
             disabled={mutation.isPending}
@@ -44,9 +48,9 @@ const DeletePostDialog = ({ open, onClose, post }: DeletePostDialogProps) => {
               "Delete"
             )}
           </Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
   );
 };
 
