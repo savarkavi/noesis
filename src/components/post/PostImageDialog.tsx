ff938ccedc8fe5c79e596c9253c5925d44fb0fe4
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-01-11 05:39:45 +0530

    style: add loading skeletons to PostImageDialog

--- src/components/post/PostImageDialog.tsx
+++ src/components/post/PostImageDialog.tsx
@@ -3,8 +3,13 @@
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import Image from "next/image";
+import { Skeleton } from "../ui/skeleton";
+import { useState } from "react";
 
 const PostImageDialog = ({ src }: { src: string }) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isDialogImageLoading, setIsDialogImageLoading] = useState(true);
+
   const handleClick = (e: React.MouseEvent) => {
     e.stopPropagation();
   };
@@ -13,23 +18,27 @@ const PostImageDialog = ({ src }: { src: string }) => {
     <Dialog>
       <DialogTrigger className="w-full" onClick={handleClick}>
         <div className="relative h-[500px] w-full">
+          {isLoading && <Skeleton className="h-full w-full rounded-xl" />}
           <Image
             src={src}
             alt="image preview"
             fill
             className="rounded-xl object-cover"
+            onLoadingComplete={() => setIsLoading(false)}
           />
         </div>
       </DialogTrigger>
       <DialogContent className="max-w-max border-none bg-transparent p-0 xl:h-full xl:max-w-fit">
         <DialogTitle className="hidden">Full Size Image</DialogTitle>
+        {isDialogImageLoading && <Skeleton className="h-screen w-screen" />}
         <Image
           src={src}
           alt="image preview"
-          className="h-screen w-full object-contain xl:h-screen xl:w-max"
+          className="h-screen w-full object-contain xl:w-max"
           width={0}
           height={0}
           unoptimized
+          onLoadingComplete={() => setIsDialogImageLoading(false)}
         />
       </DialogContent>
     </Dialog>
