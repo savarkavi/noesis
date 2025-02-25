dec2470186fa080ba0804f80005800d837b70bb8
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-02-25 15:59:35 +0530

    feat: add API routes for bookmark folders CRUD operations

---
+++ src/app/api/bookmark-folders/route.ts
@@ -0,0 +1,58 @@
+import prisma from "@/lib/prisma";
+import { getCurrentSession } from "@/lib/session";
+import { BookmarkFolder } from "@/lib/types";
+import { NextRequest } from "next/server";
+
+export async function GET() {
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const folders = await prisma.bookmarkfolder.findMany({
+      where: {
+        userId: loggedInUser.id,
+      },
+    });
+
+    return Response.json(folders);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
+
+export async function POST(req: NextRequest) {
+  const { name } = await req.json();
+
+  try {
+    const { user: loggedInUser } = await getCurrentSession();
+
+    if (!loggedInUser) {
+      return Response.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    const newFolder = await prisma.bookmarkfolder.create({
+      data: {
+        name,
+        userId: loggedInUser.id,
+      },
+      include: {
+        bookmarks: true,
+      },
+    });
+
+    const data: BookmarkFolder = {
+      id: newFolder.id,
+      name: newFolder.name,
+      totalBookmarks: newFolder.bookmarks.length,
+    };
+
+    return Response.json(data);
+  } catch (error) {
+    console.log(error);
+    return Response.json({ error: "Internal server error" }, { status: 500 });
+  }
+}
