3bd0c5d226e937ad6568244b4fcbf17aff80c10e
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-03-17 09:33:58 +0530

    refactor: remove loading skeleton for profile picture in UserProfileButton

--- src/components/UserProfileButton.tsx
+++ src/components/UserProfileButton.tsx
@@ -9,31 +9,23 @@ import {
 } from "@/components/ui/dropdown-menu";
 import Image from "next/image";
 import profilePlaceholder from "../assets/profile-placeholder.png";
-import { useState } from "react";
 import { useSession } from "@/contexts/SessionProvider";
-import { Skeleton } from "./ui/skeleton";
 import Link from "next/link";
 import { logout } from "@/app/(auth)/actions";
 
 const UserProfileButton = () => {
-  const [isProfilePictureLoading, setIsProfilePictureLoading] = useState(true);
-
   const { user } = useSession();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger className="focus:outline-none">
         <div>
-          {isProfilePictureLoading && (
-            <Skeleton className="h-10 w-10 shrink-0 rounded-full" />
-          )}
-          <div className="relative h-10 w-10 shrink-0 rounded-full">
+          <div className="relative h-10 w-10 shrink-0 rounded-full sm:h-12 sm:w-12">
             <Image
               src={user.avatarUrl || profilePlaceholder}
               alt="profile picture"
               fill
               className="rounded-full object-cover"
-              onLoadingComplete={() => setIsProfilePictureLoading(false)}
             />
           </div>
         </div>
