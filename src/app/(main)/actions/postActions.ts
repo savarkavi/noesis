b4d99e662a16c6f04e52219aada45dcdf3e96999
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-03-31 11:53:20 +0530

    feat: implement getPopularSources function to retrieve top source domains from posts

--- src/app/(main)/actions/postActions.ts
+++ src/app/(main)/actions/postActions.ts
@@ -3,7 +3,11 @@
 import prisma from "@/lib/prisma";
 import { getCurrentSession } from "@/lib/session";
 import { postDataInclude } from "@/lib/types";
-import { fetchLinkMetaData, fetchYoutubeMetaData } from "@/lib/utils";
+import {
+  fetchLinkMetaData,
+  fetchYoutubeMetaData,
+  getDomain,
+} from "@/lib/utils";
 import { postSchema } from "@/lib/validation";
 import { PostType } from "@prisma/client";
 
@@ -17,7 +21,8 @@ type CreatePostData = {
   };
   type: PostType;
   sourceTitle: string | null;
-  source: string;
+  sourceUrl: string;
+  sourceDomain: string;
   youtubeVideoId?: string | null;
   youtubeVideoTitle?: string;
   youtubeVideoDescription?: string;
@@ -29,13 +34,18 @@ export const createPost = async (data: {
   attachments: string[];
   type: PostType;
   sourceTitle: string | null;
-  source: string;
+  sourceUrl: string;
 }) => {
   const { user } = await getCurrentSession();
 
   if (!user) throw new Error("Unauthorized");
 
-  const { caption, attachments, sourceTitle, source } = postSchema.parse(data);
+  const { caption, attachments, sourceTitle, sourceUrl } =
+    postSchema.parse(data);
+
+  const sourceDomain = await getDomain(sourceUrl);
+
+  if (!sourceDomain) throw new Error("Invalid Source URL");
 
   const postData: CreatePostData = {
     caption: caption || null,
@@ -45,11 +55,12 @@ export const createPost = async (data: {
     },
     type: data.type,
     sourceTitle,
-    source,
+    sourceUrl,
+    sourceDomain,
   };
 
   if (data.type === "YOUTUBE_VIDEO") {
-    const youtubeMetadata = await fetchYoutubeMetaData(data.source);
+    const youtubeMetadata = await fetchYoutubeMetaData(data.sourceUrl);
     postData.youtubeVideoId = youtubeMetadata.id;
     postData.youtubeVideoTitle = youtubeMetadata.title;
     postData.youtubeVideoDescription = youtubeMetadata.description;
@@ -62,7 +73,7 @@ export const createPost = async (data: {
   });
 
   if (newPost.type === "ARTICLE" || newPost.type === "EXTERNAL_LINK") {
-    const linkMetadata = await fetchLinkMetaData(newPost.source);
+    const linkMetadata = await fetchLinkMetaData(newPost.sourceUrl);
 
     if (linkMetadata.title && linkMetadata.description && linkMetadata.url) {
       const createdLinkMetadata = await prisma.linkMetadata.create({
@@ -98,4 +109,25 @@ export const deletePost = async (id: string) => {
   });
 
   return deletedPost;
+};
+
+export const getPopularSources = async () => {
+  try {
+    const popularSources = await prisma.post.groupBy({
+      by: "sourceDomain",
+      _count: {
+        sourceDomain: true,
+      },
+      orderBy: {
+        _count: {
+          sourceDomain: "desc",
+        },
+      },
+      take: 5,
+    });
+
+    return popularSources;
+  } catch (error) {
+    console.log(error);
+  }
 };
