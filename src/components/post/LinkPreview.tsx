804f70154e3a4127c818269d3f8e62cb77866b81
Sushant Sharma <sushant20.sharma00@gmail.com>
2025-04-22 07:15:44 +0530

    refactor: improve structure of LinkPreview component and enhance conditional rendering

--- src/components/post/LinkPreview.tsx
+++ src/components/post/LinkPreview.tsx
@@ -4,6 +4,7 @@ import { LinkMetadata } from "@prisma/client";
 import Image from "next/image";
 import { Skeleton } from "../ui/skeleton";
 import { useState } from "react";
+import { cn } from "@/lib/utils";
 
 const LinkPreview = ({ metadata }: { metadata: LinkMetadata }) => {
   const [isLoading, setIsLoading] = useState(true);
@@ -13,19 +14,30 @@ const LinkPreview = ({ metadata }: { metadata: LinkMetadata }) => {
 
   return (
     <div className="flex h-full w-full flex-col text-white">
-      <div className="relative h-[300px] w-full rounded-lg">
-        {isLoading && <Skeleton className="h-full w-full rounded-xl" />}
-        <Image
-          src={proxyImageUrl}
-          alt="link image"
-          fill
-          className="rounded-lg object-cover"
-          onLoadingComplete={() => setIsLoading(false)}
-        />
-      </div>
-      <div className="flex flex-col gap-2 py-4 text-base">
+      {metadata.image && (
+        <div className="relative h-[300px] w-full rounded-lg">
+          {isLoading && <Skeleton className="h-full w-full rounded-xl" />}
+          (
+          <Image
+            src={proxyImageUrl}
+            alt="link image"
+            fill
+            className="rounded-lg object-cover"
+            onLoadingComplete={() => setIsLoading(false)}
+          />
+          )
+        </div>
+      )}
+      <div
+        className={cn(
+          "flex flex-col gap-2 pb-4 text-base",
+          metadata.image ? "pt-4" : "pt-0",
+        )}
+      >
         <p className="font-serif text-xl font-bold">{metadata.title}</p>
-        <p className="text-muted-foreground">{metadata.description}</p>
+        <p className="mt-2 text-sm text-muted-foreground">
+          {metadata.description}
+        </p>
         <p className="font-semibold text-muted-foreground">
           {urlWithoutProtocol}
         </p>
